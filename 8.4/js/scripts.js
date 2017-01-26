8a19828eb4f4c8ab73322eef7f752f25124d9d7d
CHRISIK <krzysztofplesniak@interia.pl>
2017-01-26 19:47:30 +0100

    3 commit

---
+++ 8.4/js/scripts.js
@@ -0,0 +1,18 @@
+// Deklaraxcja zmiennych
+alert("asas");
+
+var a = 2;
+var h = 3;
+
+// Pobranie zmiennych od użytkownika
+
+a = prompt('Wpisz podstawę trójkąta');
+h = prompt('Wpisz wysokość trójkąta');
+
+console.log('Podałeś następujace dane: podstawa = ' + a + ' wysokość = ' + h);
+
+// wyliczenie pola trójkąta
+
+var triangleArea = 1/2*a*h;
+
+console.log('Pole tójkąta wynosi = ' + triangleArea);
